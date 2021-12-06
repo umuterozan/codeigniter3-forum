33af77062419dddd1311206574a1ef4b60b8ece7
dreps <umut_erozan@hotmail.com>
2021-12-06 12:34:36 +0300

    Controller oluşturuldu

--- application/controllers/Forum.php
+++ application/controllers/Forum.php
@@ -5,10 +5,15 @@ class Forum extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('forummodel');
 	}
 
 	public function index()
 	{
-		$this->load->view('home');
+		// $this->load->view('home');
+		$data = array(
+			'category_parent' => $this->forummodel->getCategories()
+		);
+		$this->load->view('home', $data);
 	}
 }
