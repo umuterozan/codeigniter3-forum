485299e99b9b3ea71dfd0555ec3e1937099a1907
dreps <umut_erozan@hotmail.com>
2021-12-07 11:00:57 +0300

    metin düzenlemeleri

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -6,17 +6,17 @@
                         <i class="fas fa-globe"></i> Türkçe</a>
                     </div>
                     <div class="footer-row-opposite"> 
-                        <a href="#">İletişim</a>
-                        <a href="#">Terms and rules</a>
-                        <a href="#">Privacy policy</a>
-                        <a href="#">Help</a>
-                        <a href="#">Home</a>
+                        <a href="">İletişim</a>
+                        <a href="">Terms and rules</a>
+                        <a href="">Privacy policy</a>
+                        <a href="">Help</a>
+                        <a href="">Home</a>
                     </div>
                 </div>
                 <div class="footer-copyright">
-                    <a href="#">Community platform by XenForo
+                    <a href="#">Community platform by umuterozan.xyz
                         <sup>®</sup>
-                        <span class="copyright">© 2010-2021 XenForo Ltd.</span>
+                        <span class="copyright">© 2021-2022 umuterozan.xyz</span>
                     </a>
                 </div>
             </div>
