b6cd2178f039c56b2736398e2436d05842e38265
dreps <umut_erozan@hotmail.com>
2021-12-17 16:34:04 +0300

    topic iskeleti oluşturuldu

--- application/views/topic.php
+++ application/views/topic.php
@@ -37,6 +37,71 @@
                         </ul>
                     </div>
                 </div>
+                <div class="p-body-main"> <!-- buradan itibaren css yazılacak -->
+                    <div class="p-body-contentCol"></div>
+                    <div class="p-body-sidebarCol"></div>
+                    <div class="p-body-content">
+                        <div class="p-body-pageContent">
+                            <div class="block block--messages">
+                                <span class="u-anchorTarget"></span>
+                                <div class="block-outer"></div>
+                                <div class="block-container lbContainer">
+                                    <div class="block-body">
+                                        <article class="message message--post">
+                                            <span class="u-anchorTarget"></span>
+                                            <div class="message-inner">
+                                                <div class="message-cell message-cell--user">
+                                                    <section itemscope itemtype="https://schema.org/Person" class="message-user">
+                                                        <div class="message-avatar">
+                                                            <div class="message-avatar-wrapper">
+                                                                <a href="#" class="avatar avatar--m avatar--default avatar--default--dynamic" style="background-color: #ebebad; color: #b8b82e"><span class="avatar-u134-m" role="img" aria-label="Teknonely (APK Meydanı)">T</span></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="message-userDetails">
+                                                            <h4 class="message-name">
+                                                            <a href="#" class="username" dir="auto" itemprop="name">Teknonely (APK Meydanı)</a>
+                                                            </h4>
+                                                        </div>
+                                                        <span class="message-userArrow"></span>
+                                                    </section>
+                                                </div>
+                                                <div class="message-cell message-cell-main">
+                                                    <div class="message-main">
+                                                        <div class="message-attribution message-attribution--split">
+                                                            <ul class="message-attribution-main listInline">
+                                                                <li class="u-concealed">
+                                                                    <a href="#" rel="nofollow"><time class="u-dt" dir="auto" datetime="2020-12-06T15:24:40+0000" data-time="1607268280" data-date-string="6 Ara 2020" data-time-string="15:24" title="6 Ara 2020, Saat: 15:24" itemprop="datePublished">6 Ara 2020</time></a>
+                                                                </li>
+                                                            </ul>
+                                                            <ul class="message-attribution-opposite message-attribution-opposite--list">
+                                                                <li><a href="#" class="message-attribution-gadget"><i class="fa--xf far fa-share-alt" aria-hidden="true"></i></a></li>
+                                                                <li><a href="#">#1</a></li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="message-content">
+                                                            <div class="message-userContent lbContainer">
+                                                                <article class="message-body">
+                                                                    <div class="bbWrapper">
+                                                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quas iste cupiditate cum veritatis quod aut sit illo harum placeat facilis, consequatur dolores iusto maiores dicta dolorum fuga minus beatae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe voluptate voluptatum facere! Voluptatum ipsum ab, corrupti molestias dolore, similique non autem, numquam enim voluptas ad itaque magni voluptate deleniti laboriosam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eum illum minus ipsa at. Pariatur beatae ab placeat at vitae quo sint odit rerum! Aliquid dolorum nihil excepturi inventore quae.
+                                                                    </div>
+                                                                </article>
+                                                            </div>                                                            
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                </div>
+                                <div class="block-outer block-outer--after">
+                                    <div class="block-outer-opposite">
+                                        <a href="#" class="button--link button--wrap button" data-xf-click="overlay"><span class="button-text">Cevap yazmak için giriş yapmalı veya kayıt olmalısınız.</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
