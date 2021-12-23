b8cc6f4c45cee1bf8f874f7c684f148542044cdf
dreps <umut_erozan@hotmail.com>
2021-12-23 20:59:43 +0300

    hotfix

--- application/views/topic.php
+++ application/views/topic.php
@@ -6,10 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>14 Bilgisayar Hızlandırma ve FPS Arttırma Yöntemi</title>
     <link rel="icon" type="image/png" href="<?=base_url('assets/img/favicon.png');?>" sizes="32x32" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="<?=base_url('assets/css/style.min.css')?>">
-    <link rel="stylesheet" href="<?=base_url('assets/css/normalize.css')?>">
+    <?php $this->load->view('includes/style');?>
 </head>
 <body>
     <?php $this->load->view('includes/navbar');?>
@@ -103,6 +100,6 @@
     </section>
     <br>
     <?php $this->load->view('includes/footer');?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <?php $this->load->view('includes/script');?>
 </body>
 </html>
