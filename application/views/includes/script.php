4fffc33145236707b64d207c7b85cf443f11e55d
dreps <umut_erozan@hotmail.com>
2022-01-08 00:26:23 +0300

    kullanılan kütüphaneler cdn harici projeye dahil edildi

--- application/views/includes/script.php
+++ application/views/includes/script.php
@@ -1,6 +1,6 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="<?=base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
+<script src="<?=base_url('assets/js/jquery-3.6.0.min.js')?>"></script>
+<script src="<?=base_url('assets/js/toastr.min.js')?>"></script>
 <script>
     toastr.options.progressBar = true;   
 </script>
