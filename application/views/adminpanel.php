189a41622c84806aaa233920a2b23d5f1cbea90a
dreps <umut_erozan@hotmail.com>
2022-01-09 22:36:11 +0300

    admin paneline başlandı

---
+++ application/views/adminpanel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="icon" type="image/png" href="<?=base_url('assets/img/favicon.png');?>" sizes="32x32" />
+</head>
+<body>
+    
+</body>
+</html>
