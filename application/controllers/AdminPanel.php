5e63fe19fc4d4a2119cc5c25837ebc1e84864b1d
dreps <umut_erozan@hotmail.com>
2022-01-09 23:13:14 +0300

    admin panel görüntülenme kısıtlaması

--- application/controllers/AdminPanel.php
+++ application/controllers/AdminPanel.php
@@ -4,6 +4,7 @@ class AdminPanel extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->model('usermodel');
     }
 
     public function index() {
--- application/views/adminpanel.php
+++ application/views/adminpanel.php
@@ -8,6 +8,24 @@
     <link rel="icon" type="image/png" href="<?=base_url('assets/img/favicon.png');?>" sizes="32x32" />
 </head>
 <body>
-    
+    <?php if($this->session->userdata('login')) : ?>
+        <?php foreach($this->usermodel->getUserDetail(array('user_id' => $this->session->userdata('login')['user_id'])) as $key => $value) : ?>
+
+            <?php if($value->user_id == 1) : ?>
+                <h1>Admin Girişi Onaylandı</h1>
+            <?php else : ?>
+                <h1>Admin Değilsin</h1>
+            <?php endif;?>
+
+        <?php endforeach;?>
+
+    <?php else : ?>
+
+        <script>
+            alert("Lütfen bu sayfayı görüntülemek için giriş yapın!");
+            window.location.href = "<?=base_url();?>";
+        </script>
+        
+    <?php endif; ?>
 </body>
 </html>
